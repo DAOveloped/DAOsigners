903de37e4e22847bfd8699b7f6579ce8de01e423
Michael <enjoyweaver@gmail.com>
2024-02-29 18:34:37 -0600

    remove deploy button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,12 @@ import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
 import Nav from './nav';
-import Toast from './toast';
 import { Suspense } from 'react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  title: 'DAOsigner Dashboard',
   description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier.'
+    'The dashboard for the DAOsigners to view their sales and manage their profile.'
 };
 
 export default function RootLayout({
@@ -24,7 +23,6 @@ export default function RootLayout({
         </Suspense>
         {children}
         <Analytics />
-        <Toast />
       </body>
     </html>
   );
